1c78cc08d0537330ca098b7a3369c669deccaf3c
陈林 <1966069940@qq.com>
2022-05-08 19:08:34 +0800

    finish

--- perspective.js
+++ perspective.js
@@ -1,24 +1,43 @@
+
 window.addEventListener('load', () => {
-    let base = document.getElementById('canvas-base'),
-        control = document.getElementById('canvas-control'),
-        result = document.getElementById('canvas-result'),
+
+    let base = document.getElementById('canvas-base'),              // 显示原始图片
+        control = document.getElementById('canvas-control'),        // 绘制控制四边形
+        result = document.getElementById('canvas-result'),          // 显示结果
+        saveCanvas = document.getElementById('canvas-save'),        // 保存结果，设置为隐藏的canvas
         container = document.getElementById('container')
     let baseCtx = base.getContext('2d'),
         controlCtx = control.getContext('2d'),
         resultCtx = result.getContext('2d')
+
+
+    // 是否已经开始转换，用于实时更新
+    let convert_begin = false;
     
     // 可以是Image对象，也可以是HTMLVideoElement对象
-    let image;
+    let image = null;
     
     let points = [[20, 20], [20, 80], [80, 80], [80, 20]]
     let imageWidth = 100, imageHeight = 100
     let resultWidth = 300, resultHeight = 300
     let imageScale = 1, resultScale = 1
+    let startX, startY, draggingNow = -1
+    let touchId, offsetTop, offsetLeft    
 
     const imgPos2Canvas = pos => pos.map(v => v * imageScale)
     const canvasPos2Img = pos => pos.map(v => v / imageScale)
-    const resultPos2Canvas = pos => pos.map(v => v * resultScale)
-    
+
+
+    // 找到距离最近的一个触摸点
+    const findNearest = pos => points.reduce(
+        ({ minD, minIndex }, point, index) => {
+            let d = Math.sqrt((pos[0] - point[0]) ** 2 + (pos[1] - point[1]) ** 2)
+            return d < minD ? { minD: d, minIndex: index } : { minD, minIndex }
+        },
+        { minD: Infinity, minIndex: -1 }
+    ).minIndex
+
+    // 绘制四边形
     function drawControl() {
         let [width, height] = imgPos2Canvas([imageWidth, imageHeight])
         controlCtx.strokeStyle = '#FFF'
@@ -30,12 +49,6 @@ window.addEventListener('load', () => {
 
         let points1 = points.map(pos => imgPos2Canvas(pos))
 
-        controlCtx.clearRect(
-            points1[0][0],
-            points1[0][1],
-            points1[2][0] - points1[0][0],
-            points1[2][1] - points1[0][1]
-        )
         controlCtx.beginPath()
         controlCtx.moveTo(points1[3][0], points1[3][1])
         points1.forEach(([x, y]) => {
@@ -43,18 +56,25 @@ window.addEventListener('load', () => {
         })
         controlCtx.stroke()
 
-        controlCtx.fillStyle = '#FFF'
-        points1.forEach(([x, y]) => {
+        points1.forEach(([x, y], index) => {
+            controlCtx.fillStyle = index === draggingNow ? '#48adff' : '#FFF'
             controlCtx.beginPath()
             controlCtx.ellipse(x, y, 10, 10, 0, 0, 360)
             controlCtx.fill()
         })
     }
 
+    // 更新伸缩比例，canvas显示的实际是缩略图
     function updateScale() {
-        const { width, height } = container.getBoundingClientRect()
+        if (!image) return
+        const { width, height, left, top } = container.getBoundingClientRect()
         imageScale = Math.min(width / imageWidth, height / imageHeight)
-        resultScale = Math.min(width / resultWidth, height / resultHeight)
+        offsetLeft = (width - imageWidth * imageScale) / 2
+        offsetTop = top
+        // 输出距离
+        console.log("asdasd")
+        console.log(imageWidth, imageHeight)
+        console.log(resultWidth, resultHeight)
         updateCanvas()
     }
 
@@ -84,13 +104,9 @@ window.addEventListener('load', () => {
         let [width, height] = imgPos2Canvas([imageWidth, imageHeight])
         control.width = base.width = width
         control.height = base.height = height
+        // 通过openCV绘制改变清晰度
         baseCtx.drawImage(image, 0, 0, width, height)
         drawControl()
-        drawResult()
-    }
-
-    function drawResult() {
-        // code here
     }
 
     function loadImage(file) {
@@ -103,13 +119,107 @@ window.addEventListener('load', () => {
         reader.readAsDataURL(file)
     }
 
+    function handleTouchStart(e) {
+        e.preventDefault()
+        let touch = e.changedTouches[0]
+        touchId = touch.identifier
+        startX = touch.clientX
+        startY = touch.clientY
+        draggingNow = findNearest(canvasPos2Img([startX - offsetLeft, startY - offsetTop]))
+        console.log(draggingNow)
+    }
+
+    function handleTouchMove(e) {
+        e.preventDefault()
+        let touch = Array.prototype.find.call(
+            e.changedTouches,
+            touch => touch.identifier === touchId
+        )
+        if (!touch) return
+        let delta = canvasPos2Img([touch.clientX - startX, touch.clientY - startY])
+        startX = touch.clientX
+        startY = touch.clientY
+        points[draggingNow][0] += delta[0]
+        points[draggingNow][1] += delta[1]
+        updateCanvas()
+    }
+
+    function handleTouchEnd(e) {
+        e.preventDefault()
+        let touch = Array.prototype.find.call(
+            e.changedTouches,
+            touch => touch.identifier === touchId
+        )
+        draggingNow = -1
+        updateCanvas()
+        if (convert_begin)
+            convert()
+    }
+
+    // 透视变换
+    function convert() {
+        convert_begin = true;
+        let src = cv.imread(image);
+        // console.log(src);
+        let dst = new cv.Mat();
+        // console.log(src.rows, src.cols);
+        let dsize = new cv.Size(src.cols, src.rows);    // 设置变换后图片的尺寸
+        // console.log(points[0].X, points[0].Y, points[1].X, points[1].Y,points[3].X,points[3].Y,points[2].X,points[2].Y);
+        console.log(points)
+        let srcTri = cv.matFromArray(4, 1, cv.CV_32FC2, [points[0][0], points[0][1], points[1][0], points[1][1],points[2][0],points[2][1],points[3][0],points[3][1]]);
+        let dstTri = cv.matFromArray(4, 1, cv.CV_32FC2, [0, 0, src.cols, 0, src.cols, src.rows, 0, src.rows]);
+        let M = cv.getPerspectiveTransform(srcTri, dstTri);
+        // You can try more different parameters
+        cv.warpPerspective(src, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+
+        let dsize_re = new cv.Size(imageWidth * imageScale, imageHeight * imageScale);
+        let image2 = new cv.Mat(dsize_re, cv.CV_32S);
+        cv.resize(dst, image2, dsize_re);
+        console.log(dsize_re)
+        // cv.resize(image, imageWidth * imageScale, imageHeight * imageScale)
+        cv.imshow('canvas-save', dst)   // 绘制用户保存的canvas，但是不显示
+        // saveCanvas.getContext('2d').clearRect(0, 0, saveCanvas.width, saveCanvas.height)
+        cv.imshow('canvas-result', image2)
+        // saveCanvas.style.display = "none"
+        src.delete(); dst.delete(); M.delete(); srcTri.delete(); dstTri.delete();
+        console.log(image2)
+
+    }
+
+    function hello() {
+        console.log("hello world");
+        return 'hello world'
+    }
+
+    function getBase64() {
+        var res = saveCanvas.toDataURL('image/jpg');
+        // console.log(res)
+        return "asdasd"
+    }
+
+    // 浏览器可以正常保存图片
+    function save() {
+        hello()
+        saveCanvas.toBlob(function(blob) {
+            saveAs(blob, ".png");
+        })
+    }
+
     document.getElementById('img-input').addEventListener('change', e => {
         if (e.target.files.length) {
+            convert_begin = false
             loadImage(e.target.files[0])
+            resultCtx.clearRect(0, 0, result.width, result.height)
         }
     })
 
+    // 注册监听函数
     window.addEventListener('resize', updateScale)
 
-    
+    control.addEventListener('touchstart', handleTouchStart)
+    control.addEventListener('touchmove', handleTouchMove)
+    control.addEventListener('touchend', handleTouchEnd)
+
+    document.getElementById('convert').addEventListener('click', convert)
+    document.getElementById('save').addEventListener('click', save)
 })
--- readme.md
+++ readme.md
@@ -10,17 +10,19 @@
 
 
 
-## 3. canvasPaintDemo.html
+## 2. canvasPaintDemo.html
 
 ​	github上找的一个Canvas绘图的例子，可以支持绘图、保存图片、拖拽，这个例子启发了我做下面的绘制并拖动不规则四边形的方法。
 
 <img src="https://s2.loli.net/2022/05/07/Pqf6sZVuU5KrIan.png" alt="image.png" style="zoom: 50%;" />
 
-## 2. myTrans.html
+## 3. myTrans.html
 
-  	用官方的例子，改写的一个不成熟的例子，可以自己绘制梯形，并拖动。
+​	 用官方的例子，改写的一个不成熟的例子，可以自己绘制梯形，并拖动。
 
 ​     这样就得到了一个变形的哆啦A梦。
+
+​	 这里右侧会少一截，但是保存下来显示的是正常的，在下面的例子中解决了这个问题。
 
 <img src="https://s2.loli.net/2022/05/07/EjFZa7PlBkfdexY.png" alt="image.png" style="zoom: 33%;" />
 
@@ -34,6 +36,12 @@
 
 <img src="https://s2.loli.net/2022/05/07/fCKenkgzIY1EQU7.png" alt="_H@HKNI06L91O64UONQD756.png" style="zoom: 50%;" />
 
+### 5. perspective.html
+
+这个是为了动态适配Android屏幕、手机浏览器做的版本。
+
+<img src="https://s2.loli.net/2022/05/08/6WiZGCU8u7NJV5e.jpg" alt="Screenshot_20220508_112131_com.android.chrome.jpg" style="zoom: 33%;" />
+
 ### 4. openFileWithOpenCVDemo.html
 
 ​	我们有一个需求就是：从本地读取图片到image中，然后getImageData获取到像素矩阵，但是这样会报错出现了<b>跨域</b>的问题。
@@ -41,22 +49,19 @@
 ####    两个解决方案：
 
    1. 通过base64编码，在Android中获取到图片资源，然后通过编码成base64，再通过<b>WebView</b>调用JS中的方法，传递参数，在JS中解析成Image，这样的确解决了跨域的问题。
+
       但是后来使用OpenCV的方法的时候，调用这个Image对象会报错，未找到解决的办法。
-      
-   2. 通过<b><input type = "file"/ ></b>打开文件，但是同样的遇到了问题，Android会禁止对这个<b>input</b>的响应，在网上找到的解决办法是为<b>WebView重写WebClient中 
-  onShowFileChooser方法</b>，测试的确可行。
-   
-### 5. perspective.html
 
-这个是为了动态适配Android屏幕做的版本。
+   2. 通过<b><input type = "file"/ ></b>打开文件，但是同样的遇到了问题，Android会禁止对这个<b>input</b>的响应，在网上找到的解决办法是为<b>WebView重写WebClient中 onShowFileChooser方法</b>，测试的确可行。
 
+### 6. calc.html
 
+尝试实现虾苗计数的opencv.js版本，但是失败了
 
+### 7. gray.html
 
+转化成灰度图，没有使用openCV
 
-
-
-
-
+<img src="https://s2.loli.net/2022/05/07/947cxX2TCBp6nRO.png" alt="image.png" style="zoom:50%;" />
 
 
